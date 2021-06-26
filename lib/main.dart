ce2b94fcf300a15bf6add41a1bb2839879107228
mihirian <mihirvermamihir98@gmail.com>
2021-06-26 17:45:45 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mtechviral/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-          child: Center(
-              child: Container(
-        child: Text('Lets start'),
-      ))),
+      home: HomePage(),
     );
   }
 }
